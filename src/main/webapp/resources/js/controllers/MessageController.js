945ecbf463f0296ffbd41cbe37c1be394c815460
mijo <michael.jo@bchydro.com>
2015-06-26 16:32:51 -0700

    Add URL to backend

--- src/main/webapp/resources/js/controllers/MessageController.js
+++ src/main/webapp/resources/js/controllers/MessageController.js
@@ -1,10 +1,15 @@
 /**
  * Created by mijo on 2015-06-23.
  */
-app.controller('MessageController',['$scope','$http', function ($scope, $http) {
-    $http.get('resources/data/messageEvents.json').success(function(data) {
-        $scope.messageEvents = data;
-    });
+app.controller('MessageController', ['$scope', '$http', function ($scope, $http) {
+
+
+    //$scope.retrieveMessageList = function () {
+        $http.get('spring/service/messages').success(function (data) {
+            $scope.messageEvents = data;
+        });
+    //};
+
 
     //$scope.messageEvents = [
     //    {
